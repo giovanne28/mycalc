c3e27d977152536f699f422f3e0c32e4a0566e18
pvllemos <pvllemos@gmail.com>
2025-04-29 20:24:22 -0300

    subtracao

--- mycalc.py
+++ mycalc.py
@@ -1,1 +1,9 @@
-print('ola mundo')
+# Solicita dois números do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realiza a subtração
+resultado = num1 - num2
+
+# Exibe o resultado formatado
+print(f"A subtração de {num1} - {num2} é igual a {resultado:.2f}")
