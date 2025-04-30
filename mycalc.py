49b6643ef7d5644830be38134905c41cd18cc029
giovanne28 <giovanneh.araujo05@gmail.com>
2025-04-30 08:40:18 -0300

    Update mycalc.py

--- mycalc.py
+++ mycalc.py
@@ -1,18 +1,17 @@
-
+# multiplicação 
 num=  int(input('insira um numero: '))
 num2= int(input('insira um numero: '))
 resultado= num*num2
 print (f"o resultado foi {resultado}")
 
- soma
+# soma
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 soma = n1 + n2 
 print(f"A soma de {n1} mais {n2} é: {soma}")
 
-soma
 
-subtracao
+# subtração 
 # Solicita dois números do usuário
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
@@ -23,11 +22,10 @@ resultado = num1 - num2
 # Exibe o resultado formatado
 print(f"A subtração de {num1} - {num2} é igual a {resultado:.2f}")
 
-subtracao
 
-Divisao
+# divisão 
+
 num1=float(input("Digite um número:"))
 num2=float(input("Digite outro número:"))
 resultado= num1/num2
 print(resultado)
-main
